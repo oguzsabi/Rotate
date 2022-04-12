2f6afad26b24e1245e6d42c3f082a9f956cbfba7
OguzSabitay <oguz.sabitay@supplychainwizard.com>
2022-04-12 21:47:42 +0300

    - Added movement and jump logic to player - Added sprite flip logic depending on direction - Added increased gravity logic when player is in air - Added idle animation - Updated project version

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
     [SerializeField] private float _speed = 7f;
-    [SerializeField] private float _jumpForce = 100f;
+    [SerializeField] private float _jumpForce = 5f;
     private Rigidbody2D _rigidbody;
     private bool _isInAir = false;
 
@@ -13,27 +13,45 @@ public class Player : MonoBehaviour {
     public void Update() {
         this.Jump();
         this.HandleMovement();
+        this.HandleGravity();
     }
 
     private void HandleMovement() {
         float horizontal = Input.GetAxis("Horizontal");
-        Vector2 movement = new Vector2(horizontal, _rigidbody.velocity.y);
+        Vector3 movement = new Vector3(horizontal, Vector3.zero.y, Vector3.zero.z);
+        transform.position += movement * Time.deltaTime * _speed;
 
-        this._rigidbody.velocity = movement * _speed;
+        this.FlipSpriteToMovementDirection(horizontal);
+    }
+
+    private void FlipSpriteToMovementDirection(float horizontal) {
+        if (horizontal > 0) {
+            transform.localScale = new Vector3(1, 1, 1);
+        } else if (horizontal < 0) {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
     }
 
     private void Jump() {
         if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)) && !this._isInAir) {
             this._rigidbody.AddForce(new Vector2(0, _jumpForce), ForceMode2D.Impulse);
+
             this._isInAir = true;
         }
     }
 
-    public void OnCollisionEnter2D(Collision2D collision) {
-        Debug.Log("Collision");
+    private void HandleGravity() {
+        if (this._isInAir) {
+            this._rigidbody.gravityScale = 3;
 
+            return;
+        }
+
+        this._rigidbody.gravityScale = 1;
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.layer == 6) {
-            Debug.Log("Ground");
             this._isInAir = false;
         }
     }
